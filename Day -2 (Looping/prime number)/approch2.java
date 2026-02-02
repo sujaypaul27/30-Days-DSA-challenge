7409dd4a17aa44985a8bff3aa90cf6e12fa4286b
sujay <sujaypaul2711@gmail.com>
2026-02-02 23:10:28 +0530

    Added sol for approch 2

--- Day -2 (Looping/prime number)/approch2.java
+++ Day -2 (Looping/prime number)/approch2.java
@@ -1,5 +1,36 @@
-package Day -2 (Looping.prime number);
-
-public class approch2 {
-    
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+       Scanner sc =new Scanner (System.in);
+       int num =sc.nextInt();
+       
+       boolean result=true;
+       if(num==0 || num ==1)
+       {
+           result=false;
+           
+       }
+       for(int i=2;i<=Math.sqrt(num);i++)
+       {
+           if(num%i==0)
+           {
+               
+               result =false;
+               break;
+           }
+       }
+       
+       if(result)
+       {
+           System.out.println("is Prime");
+       }
+       else
+       {
+           System.out.println("is Not a Prime");
+       }
+       
+       
+    }
 }
