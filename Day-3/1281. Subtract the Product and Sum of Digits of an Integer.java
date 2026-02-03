2181166cd1771db57b8f88f54f1839c399a8932b
sujay <sujaypaul2711@gmail.com>
2026-02-03 21:53:45 +0530

    Code 1 ansr

--- Day-3/1281. Subtract the Product and Sum of Digits of an Integer.java
+++ Day-3/1281. Subtract the Product and Sum of Digits of an Integer.java
@@ -1,5 +1,17 @@
-package Day-3;
-
-public class LC {
-    
+class Solution {
+    public int subtractProductAndSum(int n) {
+        int product=1;
+        int sum=0;
+        
+        while(n!=0)
+        {
+            int digit=n%10;
+            product*=digit;
+            sum+=digit;
+            n=n/10;
+            
+        }
+        return product-sum;
+        
+    }
 }
