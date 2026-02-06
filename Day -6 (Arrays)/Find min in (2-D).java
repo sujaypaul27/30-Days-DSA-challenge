385d24bda6844d4b1a2e5cccc4f7dc8dad996da2
sujay <sujaypaul2711@gmail.com>
2026-02-06 21:48:26 +0530

    added code 3

---
+++ Day -6 (Arrays)/Find min in (2-D).java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        int[][] arr={
+            {1,2,3,4,5,6,7,0},
+            {9,8,7,6,5,4,3,2}
+        };
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=0;j<arr.length;j++)
+            {
+            if(arr[i][j]>max)
+            {
+                max=arr[i][j];
+            }
+                
+            }
+        }
+        System.out.println(max);
+    }
+}
