8a2de9c8c518c5a399deacd6d543d13129b47e69
sujay <sujaypaul2711@gmail.com>
2026-02-06 21:42:45 +0530

    Updated solution

--- Day -6 (Arrays)/Find min in array(1-D Array).java
+++ Day -6 (Arrays)/Find min in array(1-D Array).java
@@ -1,5 +1,15 @@
-package Day -6 (Arrays);
-
-public class Find min in array {
-    
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6,7,0};
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]<min)
+            {
+                min=arr[i];
+            }
+        }
+        System.out.println(min);
+    }
 }
