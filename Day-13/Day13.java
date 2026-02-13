f114e5ade6e54715283349b53e024bd35d8c2f8a
Sujay Paul <sujaypaul2711@gmail.com>
2026-02-13 22:40:57 +0530

    Code updated

--- Day-13/Day13.java
+++ Day-13/Day13.java
@@ -1,5 +1,37 @@
-package Day-13;
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
 
-public class Day13 {
-    
+        // Step 1: Find length and tail
+        ListNode temp = head;
+        int length = 1;
+
+        while (temp.next != null) {
+            temp = temp.next;
+            length++;
+        }
+
+        // Step 2: Make it circular
+        temp.next = head;
+
+        // Step 3: Effective rotations
+        k = k % length;
+
+        // Step 4: Find new tail (length - k - 1)
+        int stepsToNewTail = length - k;
+        ListNode newTail = head;
+
+        for (int i = 1; i < stepsToNewTail; i++) {
+            newTail = newTail.next;
+        }
+
+        // Step 5: Break circle
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+
+        return newHead;
+    }
 }
