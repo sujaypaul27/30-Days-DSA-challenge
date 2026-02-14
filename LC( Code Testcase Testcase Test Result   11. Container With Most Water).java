bb6ac5dcf0ebb47d64be14c69373945ce7a21d18
Sujay Paul <sujaypaul2711@gmail.com>
2026-02-14 21:52:38 +0530

    Solution updated

--- LC( Code Testcase Testcase Test Result   11. Container With Most Water).java
+++ LC( Code Testcase Testcase Test Result   11. Container With Most Water).java
@@ -1,3 +1,24 @@
-public class LC( Code Testcase Testcase Test Result   11. Container With Most Water) {
-    
+class Solution {
+    public int maxArea(int[] height) {
+        int left=0;
+        int right=height.length-1;
+        int max=0;
+        while(left<right)
+        {
+            int h=Math.min(height[left],height[right]);
+            int w=right-left;
+            int area=h*w;
+            max=Math.max(max,area);
+           while(left<right && height[left]<=h)
+           {
+            left++;
+           }
+           while(left<right && height[right]<=h)
+           {
+            right--;
+           }
+        }
+        return max;
+        
+    }
 }
