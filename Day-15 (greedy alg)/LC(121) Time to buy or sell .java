b0e591c1d0e6b1dadab29fc26634ac524316556e
Sujay Paul <sujaypaul2711@gmail.com>
2026-02-16 23:59:48 +0530

    Solution

--- Day-15 (greedy alg)/LC(121) Time to buy or sell .java
+++ Day-15 (greedy alg)/LC(121) Time to buy or sell .java
@@ -1,1 +1,19 @@
 //121. Best Time to Buy and Sell Stock
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min=Integer.MAX_VALUE;
+        int max=0;
+        for(int price: prices)
+        {
+            if(price<min)
+            {
+            min=price;
+            }
+        else{
+            int profit=price-min;
+            max=Math.max(max,profit);
+            }
+        }
+        return max;
+    }
+}
