01ebab5a46df18f907c586b0b3ef5574df3899ac
Sujay Paul <sujaypaul2711@gmail.com>
2026-02-24 21:28:16 +0530

    Solution

--- Day-22(42. Trapping Rain Water)/42. Trapping Rain Water.java
+++ Day-22(42. Trapping Rain Water)/42. Trapping Rain Water.java
@@ -1,3 +1,31 @@
-public class 42. Trapping Rain Water {
-    
+class Solution {
+    public int trap(int[] height) {
+        int left =0;
+        int right=height.length-1;
+        int leftmax=0;
+        int rightmax=0;
+        int water=0;
+        while(left<=right)
+        {
+            if(height[left]<height[right])// we can trap water on the left side
+            {
+                if(height[left]>=leftmax)
+                {
+                    leftmax=height[left];
+                }else{
+                    water+=leftmax-height[left];
+                }
+                left++;
+            }else{// we can trap water on the right side
+                if(height[right]>=rightmax)
+                {
+                    rightmax=height[right];
+                }else{
+                    water+=rightmax-height[right];
+                }
+                right--;
+            }
+        }
+        return water;
+    }
 }
