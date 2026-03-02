86cf274bc30ab71d79130bff50eb1b4492143a08
Sujay Paul <sujaypaul2711@gmail.com>
2026-03-02 23:04:21 +0530

    New code

--- Day-28(795. Number of Subarrays with Bounded Maximum)/lc(795. Number of Subarrays with Bounded Maximum).java
+++ Day-28(795. Number of Subarrays with Bounded Maximum)/lc(795. Number of Subarrays with Bounded Maximum).java
@@ -1,1 +1,23 @@
-w795. Number of Subarrays with Bounded Maximum
+class Solution {
+
+    public int numSubarrayBoundedMax(int[] nums, int left, int right) {
+        return count(nums, right) - count(nums, left - 1);
+    }
+
+    // Count subarrays where max <= k
+    private int count(int[] nums, int k) {
+        int result = 0;
+        int length = 0;
+
+        for (int num : nums) {
+            if (num <= k) {
+                length++;           // extend window
+            } else {
+                length = 0;         // break window
+            }
+            result+-= length;
+        }
+
+        return result;
+    }
+}
