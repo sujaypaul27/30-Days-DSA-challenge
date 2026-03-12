2f76cab1e530d89198503449a365b6d143cadb25
Sujay Paul <sujaypaul2711@gmail.com>
2026-03-12 22:49:35 +0530

    Comment added

--- Day-14 (container with more water)/LC(1089. Duplicate Zeros).java
+++ Day-14 (container with more water)/LC(1089. Duplicate Zeros).java
@@ -5,7 +5,7 @@ class Solution {
 
         // Count zeros
         for (int i = 0; i < n; i++) {
-            if (arr[i] == 0) zeros++;
+            if (arr[i] == 0) zeros++;// count the number of zeros in the array
         }
 
         int i = n - 1;
@@ -13,19 +13,19 @@ class Solution {
 
         // Traverse from the end
         while (i < j) {
-            if (j < n) {
-                arr[j] = arr[i];
+            if (j < n) {// if the new position is within bounds, copy the current element to the new position
+                arr[j] = arr[i];// copy the current element to the new position if it's within bounds
             }
 
-            if (arr[i] == 0) {
-                j--;
+            if (arr[i] == 0) {// if the current element is zero, we need to duplicate it
+                j--;// if the current element is zero, we need to duplicate it, so we move the new position one step back and set it to zero
                 if (j < n) {
-                    arr[j] = 0;
+                    arr[j] = 0;// set the new position to zero if it's within bounds
                 }
             }
 
-            i--;
-            j--;
+            i--;// move to the previous element in the original array
+            j--;// move to the previous position in the new array
         }
     }
 }
