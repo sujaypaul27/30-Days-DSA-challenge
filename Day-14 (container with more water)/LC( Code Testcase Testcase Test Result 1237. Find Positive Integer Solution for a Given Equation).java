aad7274f8c2a4f956ab7fe46e737a6932c6d3d42
Sujay Paul <sujaypaul2711@gmail.com>
2026-03-13 23:04:12 +0530

    Solution with exp

--- Day-14 (container with more water)/LC( Code Testcase Testcase Test Result 1237. Find Positive Integer Solution for a Given Equation).java
+++ Day-14 (container with more water)/LC( Code Testcase Testcase Test Result 1237. Find Positive Integer Solution for a Given Equation).java
@@ -10,30 +10,30 @@
  */
 
 class Solution {
-    public List<List<Integer>> findSolution(CustomFunction customfunction, int z) {
+    public List<List<Integer>> findSolution(CustomFunction customfunction, int z) // we will use two pointers to find the solution{
         
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();// initialize the result list to store the pairs of (x, y) that satisfy the equation f(x, y) == z
         
-        int x = 1;
-        int y = 1000;
+        int x = 1;// initialize the first pointer x to 1, which is the smallest positive integer
+        int y = 1000;// initialize the second pointer y to 1000, which is the largest positive integer
         
-        while (x <= 1000 && y >= 1) {
+        while (x <= 1000 && y >= 1) {// we will continue to search for pairs of (x, y) until we exhaust all possibilities within the range of 1 to 1000
             
-            int value = customfunction.f(x, y);
+            int value = customfunction.f(x, y);// calculate the value of f(x, y) using the custom function
             
-            if (value == z) {
-                result.add(Arrays.asList(x, y));
-                x++;
-                y--;
+            if (value == z) {// if the value of f(x, y) is equal to z, we have found a valid pair (x, y) that satisfies the equation
+                result.add(Arrays.asList(x, y));// add the pair (x, y) to the result list
+                x++;// move the first pointer x to the next positive integer to continue searching for other pairs
+                y--;// move the second pointer y to the previous positive integer to continue searching for other pairs
             }
-            else if (value < z) {
-                x++;
+            else if (value < z) {// if the value of f(x, y) is less than z, it means we need to increase the value of f(x, y) by increasing x, since f(x, y) is increasing with respect to x
+                x++;// move the first pointer x to the next positive integer to increase the value of f(x, y)
             }
-            else {
-                y--;
+            else {// if the value of f(x, y) is greater than z, it means we need to decrease the value of f(x, y) by decreasing y, since f(x, y) is increasing with respect to y
+                y--;// move the second pointer y to the previous positive integer to decrease the value of f(x, y)
             }
         }
         
-        return result;
+        return result;// return the list of pairs (x, y) that satisfy the equation f(x, y) == z
     }
 }
