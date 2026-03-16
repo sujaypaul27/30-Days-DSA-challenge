e6cc8309eaffb732307c8ea0dc2b65fe47161f57
Sujay Paul <sujaypaul2711@gmail.com>
2026-03-16 23:49:14 +0530

    New code

--- Day-15 (greedy alg)/LC(1346. Check If N and Its Double Exist).java
+++ Day-15 (greedy alg)/LC(1346. Check If N and Its Double Exist).java
@@ -2,17 +2,17 @@ import java.util.HashSet;
 
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();// Create a HashSet to store the elements of the array
         
-        for (int num : arr) {
+        for (int num : arr) {// Iterate through each number in the array
             
             if (set.contains(num * 2) || (num % 2 == 0 && set.contains(num / 2))) {
-                return true;
+                return true;// Check if the HashSet contains either the double of the current number or half of the current number (if it's even)
             }
             
-            set.add(num);
+            set.add(num);// Add the current number to the HashSet for future checks
         }
         
-        return false;
+        return false;// If no such pair is found after iterating through the entire array, return false
     }
 }
