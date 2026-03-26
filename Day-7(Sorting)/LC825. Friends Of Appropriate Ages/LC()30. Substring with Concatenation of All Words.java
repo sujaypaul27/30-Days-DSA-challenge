cd74607c4c0dc8e87d3fb3359ed5c7d461c16f38
Sujay Paul <sujaypaul2711@gmail.com>
2026-03-26 22:48:53 +0530

    Solution added

--- Day-7(Sorting)/LC825. Friends Of Appropriate Ages/LC()30. Substring with Concatenation of All Words.java
+++ Day-7(Sorting)/LC825. Friends Of Appropriate Ages/LC()30. Substring with Concatenation of All Words.java
@@ -1,1 +1,44 @@
-asda
+import java.util.*;
+
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> result = new ArrayList<>();
+        
+        if (s.length() == 0 || words.length == 0) return result;
+        
+        int wordLen = words[0].length();
+        int wordCount = words.length;
+        int totalLen = wordLen * wordCount;
+        
+        // Store frequency of words
+        Map<String, Integer> wordMap = new HashMap<>();
+        for (String word : words) {
+            wordMap.put(word, wordMap.getOrDefault(word, 0) + 1);
+        }
+        
+        // Try every starting point
+        for (int i = 0; i <= s.length() - totalLen; i++) {
+            Map<String, Integer> seen = new HashMap<>();
+            int j = 0;
+            
+            while (j < wordCount) {
+                int start = i + j * wordLen;
+                String sub = s.substring(start, start + wordLen);
+                
+                if (!wordMap.containsKey(sub)) break;
+                
+                seen.put(sub, seen.getOrDefault(sub, 0) + 1);
+                
+                if (seen.get(sub) > wordMap.get(sub)) break;
+                
+                j++;
+            }
+            
+            if (j == wordCount) {
+                result.add(i);
+            }
+        }
+        
+        return result;
+    }
+}
