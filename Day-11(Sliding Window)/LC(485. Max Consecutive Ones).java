848272d8be83c73da094fcf2fa59cb1474d66ef4
Sujay Paul <sujaypaul2711@gmail.com>
2026-03-29 23:38:13 +0530

    Solution updated

--- Day-11(Sliding Window)/LC(485. Max Consecutive Ones).java
+++ Day-11(Sliding Window)/LC(485. Max Consecutive Ones).java
@@ -1,5 +1,18 @@
-package Day-11(Sliding Window);
-
-public sdad {
-    
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int currentcount=0;
+        int maxcount=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1)
+            {
+                currentcount++;
+                
+            }else{
+                maxcount=Math.max(maxcount,currentcount);
+                currentcount=0;
+            }
+        }
+        maxcount=Math.max(maxcount,currentcount);
+        return maxcount;
+    }
 }
