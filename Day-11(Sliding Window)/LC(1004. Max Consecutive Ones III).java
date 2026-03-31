0ace9a090ad9fdd2ae87fbd8aaa537fa1f9b26b7
Sujay Paul <sujaypaul2711@gmail.com>
2026-03-31 22:53:59 +0530

    New code

--- Day-11(Sliding Window)/LC(1004. Max Consecutive Ones III).java
+++ Day-11(Sliding Window)/LC(1004. Max Consecutive Ones III).java
@@ -1,3 +1,22 @@
-public class LC(1004. Max Consecutive Ones III) {
-    
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int n=nums.length;
+        int left=0;
+        int max=0;
+        int zerocount=0;
+        for(int right=0;right<n;right++){
+            if(nums[right]==0)
+            {
+                zerocount++;
+            }
+            if(zerocount>k){
+                if(nums[left]==0){
+                    zerocount--;
+                }
+                left++;
+            }
+            max=right-left+1;//since right pointer is in last element and didnt end the loop
+        }
+        return max;
+    }
 }
