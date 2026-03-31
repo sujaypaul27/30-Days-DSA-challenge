b7ce615d10c2a623dccd64e96aa4341b32e731c8
Sujay Paul <sujaypaul2711@gmail.com>
2026-03-31 22:54:29 +0530

    Comments added

--- Day-11(Sliding Window)/LC(1004. Max Consecutive Ones III).java
+++ Day-11(Sliding Window)/LC(1004. Max Consecutive Ones III).java
@@ -7,13 +7,13 @@ class Solution {
         for(int right=0;right<n;right++){
             if(nums[right]==0)
             {
-                zerocount++;
+                zerocount++;//if right pointer is at zero then we are adding one zero to the window so increase the count of zeros in the window
             }
             if(zerocount>k){
                 if(nums[left]==0){
-                    zerocount--;
+                    zerocount--;//if left pointer is at zero then we are removing one zero from the window so decrease the count of zeros in the window
                 }
-                left++;
+                left++;//move left pointer to right until we have at most k zeros in the window
             }
             max=right-left+1;//since right pointer is in last element and didnt end the loop
         }
