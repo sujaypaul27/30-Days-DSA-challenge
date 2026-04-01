383db9539a2b1a8a3b8d69c264773ead80ffe6e0
Sujay Paul <sujaypaul2711@gmail.com>
2026-04-01 19:01:01 +0530

    Solution added

--- Day-11(Sliding Window)/LC(2938. Separate Black and White Balls).java
+++ Day-11(Sliding Window)/LC(2938. Separate Black and White Balls).java
@@ -1,1 +1,16 @@
-sda
+class Solution {
+    public long minimumSteps(String s) {
+        long swaps = 0;
+        long countOnes = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '1') {
+                countOnes++;
+            } else {
+                swaps += countOnes;
+            }
+        }
+
+        return swaps;
+    }
+}
