f06fdb06316213befb9a4dfb963f0757dd4fcdc3
Sujay Paul <sujaypaul2711@gmail.com>
2026-04-07 01:46:50 +0530

    New code

--- Day-9 ( Two pointer Medium)/LC(560. Subarray Sum Equals K).java
+++ Day-9 ( Two pointer Medium)/LC(560. Subarray Sum Equals K).java
@@ -1,1 +1,18 @@
-sda
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count=0;
+        int prefixsum=0;
+        HashMap<Integer,Integer> map=new HashMap<>();//to store values
+        map.put(0,1);//to avoid single k valid element edge case
+        for(int r=0;r<nums.length;r++)
+        {
+            prefixsum+=nums[r];
+            if(map.containsKey(prefixsum-k))
+            {
+                count+=map.get(prefixsum-k);
+            }
+            map.put(prefixsum,map.getOrDefault(prefixsum,0)+1);
+        }
+        return count;
+    }
+}
