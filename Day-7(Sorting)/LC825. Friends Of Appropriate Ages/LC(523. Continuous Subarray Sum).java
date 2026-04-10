a91c6f81936e86b0bb988ee907b1df7045aa439a
Sujay Paul <sujaypaul2711@gmail.com>
2026-04-10 23:27:09 +0530

    Solution added

--- Day-7(Sorting)/LC825. Friends Of Appropriate Ages/LC(523. Continuous Subarray Sum).java
+++ Day-7(Sorting)/LC825. Friends Of Appropriate Ages/LC(523. Continuous Subarray Sum).java
@@ -1,5 +1,23 @@
-package LC825. Friends Of Appropriate Ages;
-
-public class LC(523. Continuous Subarray Sum) {
-    
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int n=nums.length;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        map.put(0,-1);//for starting value
+        int prefixsum=0;
+        for(int left=0;left<n;left++)
+        {
+            prefixsum+=nums[left];
+           int rem=prefixsum%k;
+           if(map.containsKey(rem))
+           {
+            if(left-map.get(rem)>=2)
+            {
+                return true;
+            }
+           }else{
+            map.put(rem,left);
+           }
+        }
+        return false;
+    }
 }
