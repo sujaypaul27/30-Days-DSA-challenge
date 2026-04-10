85c01e2b5267fedffdd241dcc9851ecf18c70ec9
Sujay Paul <sujaypaul2711@gmail.com>
2026-04-10 23:26:10 +0530

    New code

--- Day-7(Sorting)/LC825. Friends Of Appropriate Ages/LC(Largest subarray with 0 sum).java
+++ Day-7(Sorting)/LC825. Friends Of Appropriate Ages/LC(Largest subarray with 0 sum).java
@@ -1,1 +1,25 @@
-dsda
+class Solution {
+    int maxLength(int arr[]) {
+        // code here
+        int n=arr.length;
+        int prefixsum=0;
+        int maxlength=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int left=0;left<n;left++)
+        {
+            prefixsum+=arr[left];
+           if(prefixsum==0)
+           {
+                maxlength=left+1;   
+           }
+           if(map.containsKey(prefixsum))
+           {
+               int len=left-map.get(prefixsum);
+               maxlength=Math.max(len,maxlength);
+           }else{
+               map.put(prefixsum,left);
+           }
+        }
+        return maxlength;
+    }
+}
